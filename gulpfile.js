27467e7112d10b23258881bbc7b9577324edd3c1
Amanda Michelle Marroquin Delgado <Amanda-Michelle.Marroquin-Delgado@etu.univ-grenoble-alpes.fr>
2016-10-17 10:01:47 +0200

    no message

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@
 var gulp = require('gulp');
 // Requires the gulp-sass plugin
 var sass = require('gulp-sass');
+// Requires the gulp-css-scss plugin
+var cssScss = require('gulp-css-scss');
 
 
 //JS
@@ -9,4 +11,10 @@ gulp.task('sass', function(){
   return gulp.src('assets/scss/style.scss')
     .pipe(sass())    // ici on utilise gulp-sass
     .pipe(gulp.dest('assets/css'))
+});
+
+gulp.task('css-scss', function(){
+  return gulp.src('assets/css/style.css')
+    .pipe(cssScss())
+    .pipe(gulp.dest('assets/scss'));
 });
